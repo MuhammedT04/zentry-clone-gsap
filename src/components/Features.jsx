8c2e536e2754a5b75d0fc0dc1fd3fd3cbbb12c82
Muhammed T <muhammedmhdt@gmail.com>
2025-07-08 23:05:09 +0530

    Updated title animation spacing and font styles

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -139,7 +139,7 @@ const Features = () => (
           />
         </BentoTilt>
 
-        <BentoTilt className="bento-tilt_1 row-span-1 ms-32 md:col-span-1 md:ms-0">
+        <BentoTilt className="bento-tilt_1 row-span- ms-32 md:col-span-1 md:ms-0">
           <BentoCard
             src="videos/feature-3.mp4"
             title={
